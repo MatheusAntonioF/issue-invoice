28135dd79a1f4412e08101ec1b06646c4969e53d
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-08-05 18:16:02 -0300

    feat: create mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '@nestjs/config';
+import { MongoModule } from './database/mongo.module';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongoModule],
   controllers: [],
   providers: [],
 })
---
+++ src/database/mongo.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MongoModule {}
