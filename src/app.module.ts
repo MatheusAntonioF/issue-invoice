911d5fec3c429534d2a961890197af6da5447430
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-08-08 10:24:31 -0300

    feat: resource to create a new user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
+
 import { MongoModule } from './database/mongo.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongoModule],
+  imports: [ConfigModule.forRoot(), MongoModule, UsersModule],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/users/infra/mongo/repositories/users.repository.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserRepositoryContract } from 'src/modules/users/contract/user-repository.contract';
+import { CreateUserDTO } from 'src/modules/users/dtos/create-user.dto';
+import { User, UserDocument } from '../entities/user.entity';
+
+@Injectable()
+export class UsersRepository implements UserRepositoryContract {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    const foundUser = await this.userModel.findOne({ email });
+
+    return foundUser;
+  }
+
+  async create({
+    name,
+    email,
+    password,
+  }: CreateUserDTO): Promise<UserDocument> {
+    const createdUser = await this.userModel.create({ name, email, password });
+
+    return createdUser;
+  }
+}
